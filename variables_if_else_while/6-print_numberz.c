6b88781cffa2a06ee6c8c62e623c5210f9d24513
MiguelGit20 <5178@holbertonschool.com>
2022-06-15 19:51:53 -0700

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - it prints the numbers of base 10.
+ *Return: 0
+ */
+int main(void)
+{
+int n;
+n = 48;
+while (n <= 57)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
