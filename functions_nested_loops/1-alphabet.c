1943413867996e451329e6b0bfe6d5b0351ee826
MiguelGit20 <5178@holbertonschool.com>
2022-06-17 11:16:09 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - it prints the alphabet
+ *Return: 0
+ */
+
+int main(void)
+{
+int a = 97;
+while (a <= 122)
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+return (0);
+}
