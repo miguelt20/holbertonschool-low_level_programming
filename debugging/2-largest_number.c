a1bc40a6bf3068a2315d8e8658d759316a1bad75
MiguelGit20 <5178@holbertonschool.com>
2022-06-20 20:08:03 -0700

    fixing the output

--- debugging/2-largest_number.c
+++ debugging/2-largest_number.c
@@ -9,11 +9,11 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > c && c > b)
+if (a > b && b > c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b > c && c > a)
 {
 largest = b;
 }
