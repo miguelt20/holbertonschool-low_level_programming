f4af7805b35567cba4d70f64041612cf9d01c445
MiguelGit20 <5178@holbertonschool.com>
2022-07-01 21:59:02 -0700

    square root

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _sqrt_recursion - square root
+ *Return: 0
+ *@n: variable
  *
  */
 int _sqrt_recursion(int n)
 {
 	int temp = 0, rs;
-	
+
 	rs = n / 2;
-	
+
 	if (n < 0)
-	{	
+	{
 		return (-1);
 	}
 	if (rs != temp)
 	{
 		temp = rs;
-		rs = (n/temp + temp) / 2;
+		rs = (n / temp + temp) / 2;
 	}
 	return (rs);
 }
