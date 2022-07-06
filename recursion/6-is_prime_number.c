074be81c1c60de7ea1388b12fb965adf34410168
MiguelGit20 <5178@holbertonschool.com>
2022-07-05 22:14:25 -0700

    function that checks if its a prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,11 +7,11 @@ int a_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n == 2)
 	{
-		return (0);
+		return (1);
 	}
-	return (a_prime(n, n - 1));
+	return (a_prime(2, n));
 }
 
 /**
@@ -22,13 +22,16 @@ int is_prime_number(int n)
  */
 int a_prime(int n, int i)
 {
-	if (i == 1)
-	{
-		return (1);
-	}
-	if (n % 1 == 0 && i > 0)
+	if (n < 2 || n % i == 0)
 	{
 		return (0);
 	}
-	return (a_prime(n, i - 1));
+	else if (i > n / 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (a_prime(i + 1, n));
+	}
 }
