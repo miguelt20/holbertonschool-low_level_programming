a93c00a9e067134abe80b55b748e2743c4fc8bd5
MiguelGit20 <5178@holbertonschool.com>
2022-07-07 10:28:20 -0700

    prints the number of arguments passed into it

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,6 +7,15 @@
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	int i = 0;
+
+	if (argc == 0)
+	{
+		printf("%d\n", i);
+	}
+	else
+	{
+		 printf("%d\n", argc - 1);
+	}
 	return (0);
 }
