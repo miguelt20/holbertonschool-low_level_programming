31c946e57187c393dff5e5dc046150e54bea3cd8
MiguelGit20 <5178@holbertonschool.com>
2022-07-09 21:40:28 -0700

    allocates memory

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -9,11 +9,11 @@ void *malloc_checked(unsigned int b)
 {
 	int *p;
 
-	p =(void *) malloc(sizeof(unsigned int) * b);
+	p = (void *) malloc(sizeof(unsigned int) * b);
 
 	if (p == NULL)
 	{
-		exit(3);
+		exit(98);
 	}
 
 	if (p != NULL)
