cdc659e13c6827af946c676cc24599f1c51c129d
MiguelGit20 <5178@holbertonschool.com>
2022-07-11 15:40:24 -0700

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2,  unsigned int n)
 	if (n2 >= j)
 		n2 = j;
 
-	ns = malloc(sizeof(char) * (i + n2));
+	ns = malloc(sizeof(char) * (i + n2 + 1));
 
 	if (!ns)
 		return (NULL);
@@ -46,5 +46,6 @@ char *string_nconcat(char *s1, char *s2,  unsigned int n)
 		x++;
 		z++;
 	}
+	ns[z] = '\0';
 	return (ns);
 }
