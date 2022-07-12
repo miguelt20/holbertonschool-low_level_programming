3ef02a4dc036b92411fba13b7eaa6be17cca058b
MiguelGit20 <5178@holbertonschool.com>
2022-07-11 21:56:53 -0700

    creates an array of integers

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,11 +14,11 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min + 1));
+	arr = malloc(sizeof(*arr) * (max - min + 1));
 	if (!arr)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
+	for (i = min; i <= max && min <= max; i++)
 	{
 		arr[i] = min;
 		min++;
