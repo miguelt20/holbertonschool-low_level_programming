d0ec3a959895cad73d1c91c9f29f8a1e25ee3984
MiguelGit20 <5178@holbertonschool.com>
2022-07-11 20:57:19 -0700

    allocates memory for an array

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,10 +15,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	arr = malloc(nmemb * size);
+
 	if (!arr)
 		return (NULL);
 
-	for (i = 0;i < nmemb; i++)
+	for (i = 0;i < nmemb * size; i++)
 	{
 		arr[i] = 0;
 	}
