adb212bb7b14d4c0f11ae8ce5890bdd1deee6729
root <root@DESKTOP-FQCEM9B.localdomain>
2022-07-13 22:16:06 -0500

    function that prints a struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,13 @@
  *@age:age
  *@owner:owner
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
+struct dog{
+char *name;
+float age;
+char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
