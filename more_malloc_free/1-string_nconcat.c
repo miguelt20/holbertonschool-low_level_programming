bd99a5e541c4df366f070f0a8efd53a7893ba7c6
root <root@DESKTOP-FQCEM9B.localdomain>
2022-07-14 00:04:50 -0500

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,18 +21,22 @@ char *string_nconcat(char *s1, char *s2,  unsigned int n)
 		len++;
 
 	ns = malloc(sizeof(char) * (len + 1));
-	if (!ns)
+	if (ns == NULL)
+	{
 		return (NULL);
+	}
 
 	len = 0;
 
 	for (i = 0; s1[i]; i++)
-		ns[len++] = s1[i++];
+	{
+		ns[len++] = s1[i];
+	}
 
 	for (i = 0; s2[i] && i < n; i++)
-		ns[len++] = s2[i++];
-
+	{
+		ns[len++] = s2[i];
+	}
 	ns[len] = '\0';
-
 	return (ns);
 }
