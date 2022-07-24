8ac1d497c026f9c5e67ad2bad91616371e4507bb
MiguelGit20 <5178@holbertonschool.com>
2022-07-24 15:24:32 -0700

    printing anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include "variadic_functions.h"
+void c_fun(va_list pmt);
+void i_fun(va_list pmt);
+void f_fun(va_list pmt);
+void s_fun(va_list pmt);
+/**
+ *print_all - it prints anything
+ *@format: format
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	fmt checker[] = {
+		{'c', c_fun},
+		{'i', i_fun},
+		{'f', f_fun},
+		{'s', s_fun},
+	};
+
+	int i, j;
+	char *comma = "";
+	va_list pmt;
+
+	va_start(pmt, format);
+	i = 0;
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (checker[j].ft == format[i])
+			{
+				printf("%s", comma);
+				checker[j].f(pmt);
+				comma = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(pmt);
+}
+/**
+ *c_fun - it prints a character
+ *@pmt: argument
+ */
+void c_fun(va_list pmt)
+{
+	printf("%c", va_arg(pmt, int));
+}
+
+/**
+ *i_fun - it prints an integer
+ *@pmt: argument
+ */
+void i_fun(va_list pmt)
+{
+	printf("%d", va_arg(pmt, int));
+}
+
+/**
+ *f_fun - it prints a float number
+ *@pmt: argument
+ */
+void f_fun(va_list pmt)
+{
+	printf("%f", va_arg(pmt, double));
+}
+
+/**
+ *s_fun - it prints a string
+ *@pmt: argument
+ */
+void s_fun(va_list pmt)
+{
+	char *pmts = va_arg(pmt, char*);
+
+	if (!pmts)
+		pmts = "(nill)";
+
+	printf("%s", pmts);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,28 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ *struct formats - formats
+ *@ft: arguments
+ *@f: pointer to function
+ */
+typedef struct formats
+{
+	char ft;
+	void (*f)(va_list pmt);
+} fmt;
+
+void c_fun(va_list pmt);
+void i_fun(va_list pmt);
+void f_fun(va_list pmt);
+void s_fun(va_list pmt);
 
 #endif
