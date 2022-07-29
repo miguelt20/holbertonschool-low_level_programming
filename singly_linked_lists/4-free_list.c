33cbb039d5a4c980301b2c0b8542214b60f9f15a
root <root@DESKTOP-FQCEM9B.localdomain>
2022-07-29 16:35:20 -0500

    function that frees a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_list - frees a list_t list
+ *@head: pointer to the head node
+ *Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
