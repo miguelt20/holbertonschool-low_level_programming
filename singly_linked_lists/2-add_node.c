8620b0e433546d9fc2a275f54dc881711fc77c8a
root <root@DESKTOP-FQCEM9B.localdomain>
2022-07-29 14:57:03 -0500

    function that adds a new node at the beginning of a list_t list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,6 +10,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node = NULL)
+		return (NULL);
 
 	new_node->str = strdup(str);
 
