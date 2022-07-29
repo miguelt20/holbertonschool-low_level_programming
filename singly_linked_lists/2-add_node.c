7f6047d28bb2c61d16f9573e87cd2a92bea16212
root <root@DESKTOP-FQCEM9B.localdomain>
2022-07-29 15:06:58 -0500

    adding the lenght of the string

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,15 +8,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int num_char;
 
 	new_node = (list_t *)malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	for (num_char = 0; str[num_char]; num_char++)
+		;
+
+	new_node->len = num_char;
 	new_node->str = strdup(str);
-
 	new_node->next = *head;
-
 	*head = new_node;
 
 	return (new_node);
