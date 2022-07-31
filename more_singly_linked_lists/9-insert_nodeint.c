8f28a1a97faec86bc25e12b0f54653ab29bc7c08
root <root@DESKTOP-FQCEM9B.localdomain>
2022-07-30 20:19:47 -0500

    function that inserts a new node at a given position

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (snd_index = 0; snd_index < idx; snd_index++)
 	{
+		if (idx == 0)
+		{
+			new_node->next = temp;
+			*head = new_node;
+			return (new_node);
+		}
 		if (temp->next != NULL)
 		{
 			prev_node = temp;
