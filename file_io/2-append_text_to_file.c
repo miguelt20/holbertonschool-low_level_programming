02086b859e68b988283537ad7a2711e302ce78e4
root <root@DESKTOP-FQCEM9B.localdomain>
2022-08-04 22:45:35 -0500

    function that appends text at the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,6 +12,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	while (text_content[len_text] != '\0')
+		len_text++;
+
 	if (!filename)
 	{
 		return (-1);
@@ -21,19 +24,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		file_desc = open(filename, O_RDWR | O_APPEND);
 		if (file_desc == -1)
 			return (-1);
-	}
+		if (text_content == NULL)
+			return (1);
 
-	if (text_content == NULL)
+		n_bytes = write(file_desc, text_content, len_text);
+		if (n_bytes == -1)
+			return (-1);
+
+		close(file_desc);
 		return (1);
-
-	while (text_content[len_text] != '\0')
-		len_text++;
-
-	n_bytes = write(file_desc, text_content, len_text);
-	if (n_bytes == -1)
-		return (-1);
-
-	close(file_desc);
-
+	}
 	return (1);
 }
