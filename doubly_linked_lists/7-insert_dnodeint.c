4c5e22dc591684da6e490ada604db7340c5a69b6
root <root@DESKTOP-FQCEM9B.localdomain>
2022-08-05 18:59:27 -0500

    function that inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,12 +26,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	while (idx != 1)
+	while (temp != NULL && idx != 1)
 	{
 		temp = temp->next;
 		idx--;
+		if (temp == NULL && idx == 1)
+			return (NULL);
 	}
-
+	 
 	temp2 = temp->next;
 	temp->next = new_node;
 	temp2->prev = new_node;
