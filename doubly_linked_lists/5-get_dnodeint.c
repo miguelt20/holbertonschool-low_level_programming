c929b0a3f31c04d9494831f8397a79b45ad5812b
root <root@DESKTOP-FQCEM9B.localdomain>
2022-08-05 18:09:23 -0500

    function that returns the nth node of a dlistint_t linked list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -21,8 +21,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		head = head->next;
 		temp = head;
 	}
-	if (index > snd_index)
+
+	if (temp == NULL && index > snd_index)
 		return (NULL);
+
 
 	return (temp);
 }
