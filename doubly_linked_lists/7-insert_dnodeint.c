5cc48afc5ffd1546b3d24b4316a7bca759494a63
root <root@DESKTOP-FQCEM9B.localdomain>
2022-08-05 22:32:41 -0500

    inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		s_idx++;
-		if (temp == NULL)
+		if (temp == NULL && idx > s_idx)
 			return (NULL);
 	}
 
