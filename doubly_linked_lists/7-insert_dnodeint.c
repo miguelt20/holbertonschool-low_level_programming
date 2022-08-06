b815b276b99eb609865f5a83452f6d1a79eb6ea1
root <root@DESKTOP-FQCEM9B.localdomain>
2022-08-05 22:27:45 -0500

    inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,31 +18,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
-	if (*h == NULL)
-	{
-		*h = new_node;
-		return (new_node);
-	}
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	while (temp != NULL && s_idx < idx)
 	{
 		temp = temp->next;
 		s_idx++;
 
-		if (temp == NULL && idx > s_idx)
+		if (temp == NULL)
 			return (NULL);
 	}
+
 	if (temp->next == NULL)
-	{
-		temp->next = new_node;
-		new_node->prev = temp;
-		new_node->next = NULL;
-		return (new_node);
-	}
+		return (add_dnodeint_end(h, n));
+
+	new_node->n = n;
 	temp2 = temp->prev;
 	temp->prev = new_node;
 	temp2->next = new_node;
 	new_node->next = temp;
 	new_node->prev = temp2;
+
 	return (new_node);
 }
