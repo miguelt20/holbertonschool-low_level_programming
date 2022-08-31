d530df7f89bc2c1764360f3180503eb1a5c1330c
MiguelGit20 <5178@holbertonschool.com>
2022-08-30 18:59:14 -0700

    function that adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,6 +10,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
 	hash_node_t *newnode = NULL;
+	hash_node_t *str = NULL;
 
 	if (!ht || !key)
 		return (0);
@@ -20,9 +21,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned const char *)key, ht->size);
 
+	str = (hash_node_t *)ht->array[index];
+
 	newnode->key = strdup(key);
 	newnode->value = strdup(value);
-	newnode->next = ht->array[index];
+	if (key != (const char *)str)
+		newnode->next = ht->array[index];
+
 	ht->array[index] = newnode;
 
 	return (1);
