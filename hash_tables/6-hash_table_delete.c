3cae029fb495f23fd220305e6a528f640ddca580
MiguelGit20 <5178@holbertonschool.com>
2022-09-02 10:47:34 -0700

    function that deletes a table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,32 +8,24 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int index;
 	hash_node_t *tmp = NULL;
-	hash_node_t *tmp2 = NULL;
 
 	if (!ht)
 		return;
 
-	if (ht)
+	for (index = 0; index < ht->size; index++)
 	{
-		if (ht->array)
+		if (ht->array[index])
 		{
-			for (index = 0; index <= ht->size; index++)
+			while (ht->array[index])
 			{
-				tmp = ht->array[index];
-				while (tmp)
-				{
-					tmp2 = tmp->next;
-					free(tmp->key);
-					free(tmp->value);
-					free(tmp);
-					tmp = tmp2;
-				}
-
+				tmp = ht->array[index]->next;
+				free(ht->array[index]->key);
+				free(ht->array[index]->value);
+				free(ht->array[index]);
+				ht->array[index] = tmp;
 			}
-			free(ht->array);
-			ht->array = NULL;
 		}
-		free(ht);
-		ht = NULL;
 	}
+	free(ht->array);
+	free(ht);
 }
