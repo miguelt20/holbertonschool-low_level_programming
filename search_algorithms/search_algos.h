66cf8dfa713915eebb9f7600ecb3e46dde067da3
MiguelGit20 <5178@holbertonschool.com>
2022-09-13 14:04:49 -0700

    function that searches for a value in a sorted array of integers using the Binary search algorithm

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,7 +3,8 @@
 #include <stddef.h>
 #include <stdio.h>
 
+void print_array(int *array, size_t size);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int binarySearch(int arr[], int l, int r, int x);
 #endif
